d8b2bce77d42259a31ff91fc68fc46c125db72e1
JShep1 <johnshepherd96@yahoo.com>
2018-04-11 23:09:17 -0700

    Updated readme and pick and place functions

--- README.md
+++
@@ -1,5 +0,0 @@
-The setup documents are in both of the docx files - I'll update to this README eventually.
-
-Make sure the package folder is in the ROS_PACKAGE_PATH environment variable
-
-The schunk_gripper_communication ROS package has most up to date package for schunk arm
--- schunk_gripper_communication/src/schunk_gripper_client.cpp
+++ schunk_gripper_communication/src/schunk_gripper_client.cpp
@@ -8,31 +8,151 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "schunk_gripper_client");
     schunk_gripper_communication::schunk_gripper srv;
     
+    std::vector<std::string> function_names;
+    function_names.push_back("set_motor");
+    function_names.push_back("plan_motion");
+    function_names.push_back("execute_motion");
+    function_names.push_back("create_box");
+    function_names.push_back("remove_box");
+    function_names.push_back("pick_box");
+    function_names.push_back("place_box");
 
-        if(argc == 5){
+    std::string cmd_arg = argv[1];
+
+
+    if(!function_names[0].compare(cmd_arg)){
+        //set motor function
+        if (argc == 3){
+            srv.request.function_name = argv[1];
+            srv.request.motorvalue = atof(argv[2]);
+        }else{
+            ROS_ERROR("Invalid set_motor call");
+            ROS_ERROR("Usage: schunk_gripper_client set_motor *motor value*");
+            return 0;
+        }
+    }
+    else if(!function_names[1].compare(cmd_arg)){
+        //plan motion function
+        if(argc == 2){
+            //no coordinates supplied so random plan
+            srv.request.function_name = argv[1];
+            srv.request.plan_x = -1500;
+            srv.request.plan_y = -1500;
+            srv.request.plan_z = -1500;            
+            srv.request.plan_xx = -1500;            
+            srv.request.plan_yy = -1500;            
+            srv.request.plan_zz = -1500;            
+        }else if(argc == 5){
+            //x, y, and z supplied so go there
+            srv.request.function_name = argv[1];
+            double x = atof(argv[2]);
+            double y = atof(argv[3]);
+            double z = atof(argv[4]);
+            srv.request.plan_xx = -1500;            
+            srv.request.plan_yy = -1500;            
+            srv.request.plan_zz = -1500;            
+            
+            srv.request.plan_x = x; 
+            srv.request.plan_y = y; 
+            srv.request.plan_z = z; 
+
+        }else if(argc == 8){
+            
+            srv.request.function_name = argv[1];
+            double x = atof(argv[2]);
+            double y = atof(argv[3]);
+            double z = atof(argv[4]);
+            double xx = atof(argv[5]);
+            double yy = atof(argv[6]);
+            double zz = atof(argv[7]);
+            
+            srv.request.plan_x = x; 
+            srv.request.plan_y = y; 
+            srv.request.plan_z = z; 
+            srv.request.plan_xx = xx; 
+            srv.request.plan_yy = yy; 
+            srv.request.plan_zz = zz; 
+            
+        }else{
+            //Invalid number of args
+            ROS_ERROR("Invalid plan_motion call");
+            ROS_ERROR("For random plan:");
+            ROS_ERROR("Usage: schunk_gripper_client plan_motion");
+            ROS_ERROR("For specified plan:");
+            ROS_ERROR("Usage: schunk_gripper_client plan_motion *x_val* *y_val* *z_val*");
+            return 0;
+            
+        }
+    }
+    else if(!function_names[2].compare(cmd_arg)){
+        //execute motion function
+        srv.request.function_name = argv[1];
+
+    }
+    else if(!function_names[3].compare(cmd_arg)){
+        //create box function
+        srv.request.function_name = argv[1];
+        if(argc == 6){
+
             double box_x = atof(argv[2]);
             double box_y = atof(argv[3]);
             double box_z = atof(argv[4]);
-            
-            if(box_x <= 0 || box_y <= 0 || box_z <= 0){
-                ROS_INFO("Invalid values input. Try again.");
-                return 0;
-            }
+            std::string id = argv[5];
 
             srv.request.box_x = box_x; 
             srv.request.box_y = box_y; 
             srv.request.box_z = box_z; 
-        }else if (argc == 3){
-            srv.request.motorvalue = atof(argv[2]);
-            return 1;
-        }else if (argc == 2){
-            srv.request.motorvalue = -1;
+            srv.request.id = id;
         }else{
-            ROS_INFO("usage: schunk_gripper_client *function name* OPTIONAL-*motor value*");
+            ROS_ERROR("Invalid create_box call");
+            ROS_ERROR("Usage: schunk_gripper_client create_box *x_val* *y_val* *z_val* *id*");
             return 0;
         }
-            srv.request.function_name = argv[1];
-    
+
+    }
+    else if(!function_names[4].compare(cmd_arg)){
+        //remove box function
+        srv.request.function_name = argv[1];
+        if (argc == 3){
+            srv.request.id = argv[2];
+        }else{
+            ROS_ERROR("Invalid remove_box call");
+            ROS_ERROR("Usage: schunk_gripper_client remove_box *id*");
+            return 0;
+        }
+
+    }
+    else if(!function_names[5].compare(cmd_arg)){
+        //pick box function
+        srv.request.function_name = argv[1];
+        if (argc == 3){
+            srv.request.id = argv[2];
+        }else{
+            ROS_ERROR("Invalid pick_box call");
+            ROS_ERROR("Usage: schunk_gripper_client pick_box *id*");
+            return 0;
+        }
+
+    }
+    else if(!function_names[6].compare(cmd_arg)){
+        //place box function
+        srv.request.function_name = argv[1];
+        if (argc == 3){
+            srv.request.id = argv[2];
+        }else{
+            ROS_ERROR("Invalid place_box call");
+            ROS_ERROR("Usage: schunk_gripper_client place_box *id*");
+            return 0;
+        }
+
+    }
+
+    else{
+        ROS_ERROR("Invalid function call.");    
+        ROS_ERROR("Valid function calls are set_motor, plan_motion, execute_motion, create_box");   
+        return 0; 
+    }
+
     //setenv("PYTHONPATH",".",1);
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<schunk_gripper_communication::schunk_gripper>("choose_function");
